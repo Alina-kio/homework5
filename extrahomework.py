0df33f31796e92b27ef08247844c51103818827f
Alina-kio <95016284+Alina-kio@users.noreply.github.com>
2022-05-23 22:59:18 +0600

    Update extrahomework.py

--- extrahomework.py
+++ extrahomework.py
@@ -3,5 +3,5 @@ while True:
         h = seconds//3600
         m = (seconds-h*3600)//60
         s = seconds-(h*3600)-(m*60)
-        print("%02d:%02d:%02d" % (h,m,s) if seconds <= 359999 else 'Время превышает допустимое значение')
+        print("%02d:%02d:%02d" % (h,m,s) if 0 < seconds <= 359999 else 'Время превышает допустимое значение или вы ввели отрицательное число!"')
     make_readable(int(input('enter seconds: ')))
